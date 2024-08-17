4f19a859751ec8d01171d254bdaae80937b71d8f
Revanth srinivaslu Bolla <113007630+Revanth-03@users.noreply.github.com>
2024-08-17 05:43:47 +0000

    added state to create post component

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -2,27 +2,42 @@ import { useState } from "react";
 
 export default function CreatePost() {
   const [title, setTitle] = useState("");
-  const [subtitle, setSubTitle] = useState("");
-  const [textare, setTextArea] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [textArea, setTextArea] = useState("");
 
-  function handleSubmit(e) {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("title", title, "subtitle", subTitle, "textarea", textArea);
+  }
   return (
     <div className="create-post">
       <h1>Create Post</h1>
       <form onSubmit={handleSubmit} className="create-post_from">
         <div className="title">
           <label htmlFor="">Title</label>
-          <input type="text" />
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+          />
         </div>
 
         <div className="sub-title">
           <label htmlFor="">SubTitle</label>
-          <input type="text" />
+          <input
+            value={subTitle}
+            onChange={(e) => setSubTitle(e.target.value)}
+            type="text"
+          />
         </div>
 
         <div className="feedback">
           <label htmlFor="">Comment</label>
-          <textarea rows={10}></textarea>
+          <textarea
+            value={textArea}
+            onChange={(e) => setTextArea(e.target.value)}
+            rows={10}
+          ></textarea>
         </div>
         <button type="submit">Create Post</button>
       </form>
